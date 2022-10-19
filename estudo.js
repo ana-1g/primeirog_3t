6e774a386210191b972ead51944eee277cf2425c
ana-1g <108944811+ana-1g@users.noreply.github.com>
2022-10-19 11:48:25 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,12 +1,29 @@
-function total(){
-  let c = document.getElementById("capital").value;
-  let j = document.getElementById("juros").value;
-  let t = document.getElementById("meses").value;
-  let m = 0;
-  for(let i = 1; i <= t; i++){
-      m = c * (1 + (j/100));
-      document.write("Valor no mês " + i + " = "+ m + "<br>");
-      c = m;
-  }
-  document.write("Montante: " + m)
-}
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro G</title>
+    <script src="estudo.js"></script>
+</head>
+<body>
+
+    Capital: <input id="capital" type="text"><br>
+    Juros mensais: <input id="juros" type="text"><br>
+    Quantidade de meses: <input id="meses" type="text"><br>
+    <button onclick="total()">Calcular</button>
+
+    <br><br><br>
+    <span id="resultado"><span><br><br>
+    Valor 1: <input id="v1" type"text"><br>
+    Valor 2: <input id="v2" type"text"><br>
+      <button onclick="soma()">Soma</button>
+      <button onclick="subtracao()">Subtração</button>
+      <button onclick="divisao()">Divisão</button>
+      <button onclick="multiplicacao()">Multiplicação</button>
+      <button onclick="porcentagem()">Porcentagem</button>
+
+</body>
+</html>
+
