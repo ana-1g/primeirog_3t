ea89170d921578a5363eba80b96a123e03ae2f0d
ana-1g <108944811+ana-1g@users.noreply.github.com>
2022-10-26 11:55:42 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -2,6 +2,24 @@ function total(){
   let c = document.getElementById("capital").value;
   let j = document.getElementById("juros").value;
   let t = document.getElementById("meses").value;
+if (!Number(c)){
+  alert("0 valor do capital deve ser um número.");
+  document.getElementById("capital").value = "";
+  document.getElementById("capital").focus();
+  return  
+}
+if (!Number(j)){
+  alert("0 valor dos juros deve ser um número.");
+  document.getElementById("juros").value = "";
+  document.getElementById("juros").focus();
+  return  
+}
+if (!Number(c)){
+  alert("0 valor dos meses deve ser um número.");
+  document.getElementById("meses").value = "";
+  document.getElementById("meses").focus();
+  return  
+}
   let m = 0;
   for(let i = 1; i <= t; i++){
       m = c * (1 + (j/100));
@@ -10,40 +28,3 @@ function total(){
   }
   document.write("Montante: " + m)
 }
-
-function soma(){
-  let val1 = document.getElementById("v1").value;
-  let val2 = document.getElementById("v2").value;
-  let r = Number(val1) + Number(val2);
-  document.getElementById("resultado").innerHTML = r;
-  
-}
-
-function subtracao(){
-  let val1 = document.getElementById("v1").value;
-  let val2 = document.getElementById("v2").value;
-  let r = Number(val1) - Number(val2);
-  document.getElementById("resultado").innerHTML =r;
-  }
-
-  function divisao(){
-    let val1 = document.getElementById("v1").value;
-    let val2 = document.getElementById("v2").value;
-    let r = Number(val1) / Number(val2);
-    document.getElementById("resultado").innerHTML =r;
-    }
-  
-    function multiplicacao(){
-      let val1 = document.getElementById("v1").value;
-      let val2 = document.getElementById("v2").value;
-      let r = Number(val1) * Number(val2);
-      document.getElementById("resultado").innerHTML =r;
-      }
-      function porcentagem(){
-        let val1 = document.getElementById("v1").value;
-        let val2 = document.getElementById("v2").value;
-        let c = 100
-        let p = Number(val2)  / Number(c)
-        let r = Number(val1) * Number(p);
-        document.getElementById("resultado").innerHTML =r;
-        } 
